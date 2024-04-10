da594d4d8fc9fd36d084a0975268a4e1d2502039
hyewon93 <baehw93@gmail.com>
2024-04-10 13:15:37 -0700

    Add AppContext and Toast

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
     firstName: string;
@@ -11,15 +13,19 @@ export type RegisterFormData = {
 };
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const { showToast } = useAppContext();
     const { register, watch, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
 
     // useMutation: a hook provided by React Query for server-side mutations like POST, PUT, DELETE requests.
     const mutation = useMutation(apiClient.register, {
         onSuccess: () => {
-            console.log("registration successful!");
+            showToast({message: "User registered successfully!", type: "SUCCESS"});
+            navigate("/");
         },
         onError: (error: Error) => {
-            console.log(error.message);
+            showToast({message: error.message, type: "ERROR"});
         }
     });
 
