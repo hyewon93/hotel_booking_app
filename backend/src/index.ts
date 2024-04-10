0ef59c2e4f4ee0ea0dedd76052b730cf3732c9b6
hyewon93 <baehw93@gmail.com>
2024-04-10 10:27:23 -0700

    Integrate User Registration API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,19 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
-import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+import "dotenv/config";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
+// express setup
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({message: "hello from express endpoint!"});
-});
+// endpoint setup
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
     console.log("server is running on localhost:7000");
