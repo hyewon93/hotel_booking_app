8e1d516ac819073371325eb4c17f88c6aba2391e
hyewon93 <baehw93@gmail.com>
2024-04-10 13:53:49 -0700

    Login status setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import "dotenv/config";
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth';
 import userRoutes from './routes/users';
@@ -10,9 +11,13 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 // express setup
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
 
 // endpoint setup
 app.use("/api/auth", authRoutes);
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,6 +3,7 @@ import { check, validationResult } from "express-validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
@@ -38,6 +39,10 @@ router.post("/login", [
         console.log(error);
         res.status(500).json({ message: "Something went wrong" });
     }
+});
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+    res.status(200).send({userId: req.userId});
 });
 
 export default router;
