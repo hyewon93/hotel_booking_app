b5d324a0efccaeb8622ba6c4225c1e7946da9374
hyewon93 <baehw93@gmail.com>
2024-04-27 11:35:01 -0700

    Fish Booking Confirmation Page

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,20 +1,47 @@
 import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
 import BookingForm from "../forms/BookingForm/BookingForm";
+import { useSearchContext } from "../contexts/SearchContext";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import BookingDetailSummary from "../components/BookingDetailSummary";
 
 const Booking = () => {
-  const { data: currentUser } = useQuery("fetchCurrentUser", apiClient.fetchCurrentUser);
+    const search = useSearchContext();
+    const { hotelId } = useParams();
 
-  console.log(currentUser?.email);
+    const [numberOfNights, setNumberOfNights] = useState<number>(0);
 
-  return (
-    <div className="grid md:grid-cols-[1fr_2fr]">
-        <div className="bg-green-200">BOOKING DETAILS SUMMARY</div>
-        {currentUser && (
-            <BookingForm currentUser={currentUser} />
-        )}
-    </div>
-  );
+    useEffect(() => {
+        if(search.checkIn && search.checkOut) {
+            const nights = Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) / (1000 * 60 * 60 * 24);
+
+            setNumberOfNights(Math.ceil(nights));
+        }
+    }, [search.checkIn, search.checkOut]);
+
+    const { data: hotel } = useQuery("fetchHotelById", () => apiClient.fetchHotelById(hotelId as string), { enabled: !!hotelId });
+    const { data: currentUser } = useQuery("fetchCurrentUser", apiClient.fetchCurrentUser);
+
+    if(!hotel) {
+        return <></>;
+    }
+
+    return (
+        <div className="grid md:grid-cols-[1fr_2fr]">
+            <BookingDetailSummary 
+                checkIn={search.checkIn} 
+                checkOut={search.checkOut} 
+                adultCount={search.adultCount} 
+                childCount={search.childCount} 
+                numberOfNights={numberOfNights} 
+                hotel={hotel}
+            />
+            {currentUser && (
+                <BookingForm currentUser={currentUser} />
+            )}
+        </div>
+    );
 }
 
 export default Booking;
